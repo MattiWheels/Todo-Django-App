5f64b36bb6f6d9ddfdc61d8bbb4307aa9d973466
matti <lime811@gmail.com>
2020-12-20 14:51:09 -0800

    set static directory and made some styling changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 try:
     from .secret_key import keychain
 except ImportError:
@@ -124,4 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
